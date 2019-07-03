ddbf23935baa3ee58da4948b82006900957ca899
Nerevarishe <JIyHaTuK@gmail.com>
2019-07-03 12:01:27 +0300

    Adding Modal with Backdrop

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,18 +1,34 @@
 import React, {Component} from 'react';
-import NavigationBar from '../components/Navigation/NavigationBar/NavigationBar'
+
+import Modal from '../UI/Modal/Modal';
+
+import NavigationBar from '../components/Navigation/NavigationBar/NavigationBar';
 import ContactsApp from '../containers/ContactsApp/ContactsApp';
 
 class Layout extends Component {
+    state = {
+        showModal: true
+    };
+
+    // ModalHandler = (prevState) => {
+    //     if (prevState.showModal !== this.state.showModal) {
+    //         this.setState({showModal: !this.state.showModal})
+    //     }
+    // };
+
     render() {
         return (
-            <div>
-                <NavigationBar>
-                    {this.props.children}
-                </NavigationBar>
-                <hr/>
-                <div className="row">
-                    <div className='col'>
-                        <ContactsApp />
+            <div className='row'>
+                <div className="col">
+                    <Modal show={this.state.showModal} modalClosed={() => this.setState({showModal: false})}/>
+                    <NavigationBar>
+                        {this.props.children}
+                    </NavigationBar>
+                    <hr/>
+                    <div className="row">
+                        <div className='col'>
+                            <ContactsApp/>
+                        </div>
                     </div>
                 </div>
             </div>
